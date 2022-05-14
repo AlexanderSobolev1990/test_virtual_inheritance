fd7516f8261d4e95b8c9888e2950c9d38e2ef48d
alex <alexandersobolev1990@yandex.ru>
2022-05-14 12:00:09 +0300

    first commit

---
+++ classes.h
@@ -0,0 +1,226 @@
+#ifndef CLASSES_H
+#define CLASSES_H
+
+#include <iostream>
+#include <armadillo>
+//----------------------------------------------------------------------------------------------------------------------
+template<size_t SizeX, size_t SizeY>
+class A
+{
+public:
+    A() : IntegerA( 0 ), MatrixA( arma::zeros( SizeX, SizeY ) )
+    {
+        std::cout << "default constructor A" << std::endl;
+    }
+
+    A( const A &other )
+    {
+        std::cout << "copy constructor A" << std::endl;
+        this->IntegerA = other.IntegerA;
+        this->MatrixA = other.MatrixA;
+    }
+
+    A& operator=( A other ) // Да, именно так, без &
+    {
+        std::cout << "copy assignment A" << std::endl;        
+        swap( *this, other );
+        return *this;
+    }
+
+    A( A &&other ) noexcept
+        : IntegerA( 0 ), MatrixA( arma::zeros( SizeX, SizeY ) )
+    {
+        std::cout << "move constructor A" << std::endl;
+        swap( *this, other );
+    }
+
+    A& operator=( A &&other ) noexcept
+    {
+        std::cout << "move assignment A" << std::endl;
+        swap( *this, other );
+        return *this;
+    }
+
+    friend void swap( A<SizeX, SizeY> &lhs, A<SizeX, SizeY> &rhs ) noexcept
+    {
+        std::cout << "swap A" << std::endl;
+
+        // Свап полей класса
+        std::swap( lhs.IntegerA, rhs.IntegerA );
+        std::swap( lhs.MatrixA, rhs.MatrixA );
+    }
+
+    // Поля
+    int IntegerA;
+    arma::mat MatrixA;
+};
+//----------------------------------------------------------------------------------------------------------------------
+template<size_t SizeX, size_t SizeY>
+class B : virtual public A<SizeX, SizeY>
+{
+public:
+//    using A<SizeX, SizeY>::A;
+
+    B() : A<SizeX, SizeY>(),
+        IntegerB( 0 ), MatrixB( arma::zeros( SizeX, SizeY ) )
+    {
+        std::cout << "default constructor B" << std::endl;
+    }
+
+    B( const B &other ) : A<SizeX, SizeY>( other )
+    {
+        std::cout << "copy constructor B" << std::endl;
+        this->IntegerB = other.IntegerB;
+        this->MatrixB = other.MatrixB;
+    }
+
+    B& operator=( B other ) // Да, именно так, без &
+    {
+        std::cout << "copy assignment B" << std::endl;        
+        swap( *this, other );
+        return *this;
+    }
+
+    B( B &&other ) : A<SizeX, SizeY>(),
+        IntegerB( 0 ), MatrixB( arma::zeros( SizeX, SizeY ) )
+    {
+        std::cout << "move constructor B" << std::endl;
+        swap( *this, other );
+    }
+
+    B& operator=( B &&other )
+    {
+        std::cout << "move assignment B" << std::endl;
+        swap( *this, other );
+        return *this;
+    }
+
+    friend void swap( B<SizeX, SizeY> &lhs, B<SizeX, SizeY> &rhs )
+    {
+        std::cout << "swap B" << std::endl;
+        swap( static_cast<A<SizeX, SizeY>&>( lhs ), static_cast<A<SizeX, SizeY>&>( rhs ) ); // Свап предка 1 порядка
+
+        // Свап полей класса
+        std::swap( lhs.IntegerB, rhs.IntegerB );
+        std::swap( lhs.MatrixB, rhs.MatrixB );
+    }
+
+    // Поля
+    int IntegerB;
+    arma::mat MatrixB;
+};
+//----------------------------------------------------------------------------------------------------------------------
+template<size_t SizeX, size_t SizeY>
+class C : virtual public A<SizeX, SizeY>
+{
+public:
+//    using A<SizeX, SizeY>::A;
+
+    C() : A<SizeX, SizeY>(),
+        IntegerC( 0 ), MatrixC( arma::zeros( SizeX, SizeY ) )
+    {
+        std::cout << "default constructor C" << std::endl;
+    }
+
+    C( const C &other ) : A<SizeX, SizeY>( other )
+    {
+        std::cout << "copy constructor C" << std::endl;
+        this->IntegerC = other.IntegerC;
+        this->MatrixC = other.MatrixC;
+    }
+
+    C& operator=( C other ) // Да, именно так, без &
+    {
+        std::cout << "copy assignment C" << std::endl;        
+        swap( *this, other );
+        return *this;
+    }
+
+    C( C &&other ) noexcept
+        : IntegerC( 0 ), MatrixC( arma::zeros( SizeX, SizeY ) )
+    {
+        std::cout << "move constructor C" << std::endl;
+        swap( *this, other );
+    }
+
+    C& operator=( C &&other ) noexcept
+    {
+        std::cout << "move assignment C" << std::endl;
+        swap( *this, other );
+        return *this;
+    }
+
+    friend void swap( C<SizeX, SizeY> &lhs, C<SizeX, SizeY> &rhs ) noexcept
+    {
+        std::cout << "swap C" << std::endl;
+        swap( static_cast< A<SizeX, SizeY> &>( lhs ), static_cast< A<SizeX, SizeY> &>( rhs ) ); // Свап предка 1 порядка
+
+        // Свап полей класса
+        std::swap( lhs.IntegerC, rhs.IntegerC );
+        std::swap( lhs.MatrixC, rhs.MatrixC );
+    }
+
+    // Поля
+    int IntegerC;
+    arma::mat MatrixC;
+};
+//----------------------------------------------------------------------------------------------------------------------
+template<size_t SizeX, size_t SizeY>
+class D : public B<SizeX, SizeY>, public C<SizeX, SizeY>
+{
+public:
+//    using B<SizeX, SizeY>::B;
+//    using C<SizeX, SizeY>::C;
+
+    D() : B<SizeX, SizeY>(), C<SizeX, SizeY>(),
+        IntegerD( 0 ), MatrixD( arma::zeros( SizeX, SizeY ) )
+    {
+        std::cout << "default constructor D" << std::endl;
+    }
+
+    D( const D &other ) : B<SizeX, SizeY>( other ), C<SizeX, SizeY>( other ), A<SizeX, SizeY>( other )
+    {
+        std::cout << "copy constructor D" << std::endl;
+        this->IntegerD = other.IntegerD;
+        this->MatrixD = other.MatrixD;
+    }
+
+    D& operator=( D other ) // Да, именно так, без &
+    {
+        std::cout << "copy assignment D" << std::endl;        
+        swap( *this, other );
+        return *this;
+    }
+
+    D( D &&other ) noexcept
+        : IntegerD( 0 ), MatrixD( arma::zeros( SizeX, SizeY ) )
+    {
+        std::cout << "move constructor D" << std::endl;
+        swap( *this, other );
+    }
+
+    D& operator=( D &&other ) noexcept
+    {
+        std::cout << "move assignment D" << std::endl;
+        swap( *this, other );
+        return *this;
+    }
+
+    friend void swap( D<SizeX, SizeY> &lhs, D<SizeX, SizeY> &rhs ) noexcept
+    {
+        std::cout << "swap D" << std::endl;
+        swap( static_cast< B<SizeX, SizeY> &>( lhs ), static_cast< B<SizeX, SizeY> &>( rhs ) ); // Свап предка 1 порядка
+        swap( static_cast< C<SizeX, SizeY> &>( lhs ), static_cast< C<SizeX, SizeY> &>( rhs ) ); // Свап предка 1 порядка
+        swap( static_cast< A<SizeX, SizeY> &>( lhs ), static_cast< A<SizeX, SizeY> &>( rhs ) ); // Свап предка 2 порядка
+
+        // Свап полей класса
+        std::swap( lhs.IntegerD, rhs.IntegerD );
+        std::swap( lhs.MatrixD, rhs.MatrixD );
+    }
+
+    // Поля
+    int IntegerD;
+    arma::mat MatrixD;
+};
+
+#endif // CLASSES_H
---
+++ main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <map>
+
+#include <classes.h>
+
+int main()
+{
+    std::cout << "test_virtual_inheritance" << std::endl;
+
+    arma::mat M1 = { { 0, 1, 2 },
+                     { 3, 4, 5 } };
+    arma::mat M2 = { { 6,  7,  8 },
+                     { 9, 10, 11 } };
+
+    //------------------------------------------------------------------------------------------------------------------
+    std::cout << "\n test 1 - Without romb \n" << std::endl;
+
+    B<2, 3> instanceB1;
+    instanceB1.IntegerA = 1;
+    instanceB1.MatrixA = M1;
+
+    B<2, 3> instanceB2;
+    instanceB2.IntegerA = 2;
+    instanceB2.MatrixA = M2;
+
+    instanceB1.MatrixA.print( "B1.MatrixA before:" );
+    instanceB2.MatrixA.print( "B2.MatrixA before:" );
+
+    instanceB1 = instanceB2; // То, ради чего всё вот это вот делается
+    instanceB2.MatrixA.zeros();
+
+    instanceB1.MatrixA.print( "B1.MatrixA after:" );
+    instanceB2.MatrixA.print( "B2.MatrixA after:" );
+
+    //------------------------------------------------------------------------------------------------------------------
+    std::cout << "\n test 2 - With romb \n" << std::endl;
+
+    D<2,3> instanceD1;
+    instanceD1.IntegerA = 1;
+    instanceD1.MatrixA = M1;
+
+    D<2,3> instanceD2;
+    instanceD2.IntegerA = 2;
+    instanceD2.MatrixA = M2;
+
+    instanceD1.MatrixA.print( "D1.MatrixA before:" );
+    instanceD2.MatrixA.print( "D2.MatrixA before:" );
+
+    instanceD1 = instanceD2; // То, ради чего всё вот это вот делается
+
+    instanceD1.MatrixA.print( "D1.MatrixA after:" );
+    instanceD2.MatrixA.print( "D2.MatrixA after:" );
+
+    //------------------------------------------------------------------------------------------------------------------
+    std::cout << "\n test 3 - With romb, with map \n" << std::endl;
+
+    instanceD1.IntegerA = 1;
+    instanceD1.MatrixA = M1;
+
+    instanceD2.IntegerA = 2;
+    instanceD2.MatrixA = M2;
+
+    std::map<int, D<2,3>> myMap;
+    myMap.insert( std::make_pair( 1, instanceD1 ) );
+    myMap.insert( std::make_pair( 2, instanceD2 ) );
+
+    myMap.at( 1 ).MatrixA.print();
+    myMap.at( 2 ).MatrixA.print();
+
+    myMap.at( 1 ) = myMap.at( 2 ); // То, ради чего всё вот это вот делается
+
+    for( auto &a : myMap ) {
+        std::cout << ( a.first ) << std::endl;
+        ( a.second ).MatrixA.print();
+    }
+
+    myMap.erase( 2 );
+
+    for( auto &a : myMap ) { // new
+        std::cout << ( a.first ) << std::endl;
+        ( a.second ).MatrixA.print();
+    }
+
+    return 0;
+}
